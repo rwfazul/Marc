a91a6e64fdf32d6c1b942e8399520dac276508aa
rwfazul <rwfazul@inf.ufsm.br>
2018-08-26 02:38:00 -0300

    add front-end base

--- core/app/urls.py
+++ core/app/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('form/', views.form, name='form')
+	path('dashboard/', views.dashboard, name='dashboard'),
+	path('charts/', views.charts, name='charts'),
+	path('charts-two/', views.charts_two, name='charts_two'),
+	path('tables/', views.tables, name='tables'),
+	path('forms/', views.forms, name='forms'),
+	path('fix/', views.fix, name='fix'),
 ]
--- core/app/views.py
+++ core/app/views.py
@@ -4,7 +4,22 @@ from django.shortcuts import render
 
 
 def home(request):
+	return render(request, 'login.html')
+
+def dashboard(request):
 	return render(request, 'app_templates/home.html')
 
-def form(request):
+def charts(request):
+	return render(request, 'app_templates/charts.html')
+
+def charts_two(request):
+	return render(request, 'app_templates/charts-two.html')
+
+def tables(request):
+	return render(request, 'app_templates/tables.html')
+
+def forms(request):
 	return render(request, 'app_templates/forms.html')
+
+def fix(request):
+	return render(request, 'app_templates/fix.html')
